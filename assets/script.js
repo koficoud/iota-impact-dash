1ed60ac605466322c01613f129bd4f8b541736e9
Fernando López Moreno <koficoud@gmail.com>
2021-08-21 11:59:39 -0500

    docs: script.js updated

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,47 @@
+/**
+ * Init app
+ */
 document.addEventListener('DOMContentLoaded', function () {
-    const dashAppContent = document.querySelector('body');
+    const bodyElement = document.querySelector('body');
 
     // Create an observer instance
     const observer = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
+        mutations.forEach(function () {
             domRefreshed();
         });
     });
-    // Configuration of the observer
-    const config = {
+
+    // Start observation to the target
+    observer.observe(bodyElement, {
         childList: true,
         characterData: true,
         subtree: true
-    };
-
-    // Start observation to the target
-    observer.observe(dashAppContent, config);
+    });
 });
 
 /**
- * Executes every React changes the DOM.
- * All elements on window are available.
+ * Executes every that React changes the DOM
+ * All elements on the window are available
  */
 function domRefreshed() {
     // Top companies modal button
     const topCompaniesBtn = document.querySelector('#top-companies');
+    // All iframes that actually are used on top 10 companies modal
+    const iframes = document.querySelectorAll('iframe');
+
 
     if (topCompaniesBtn !== null && !topCompaniesBtn.classList.contains('loaded-element')) {
         // Add loaded-element class to prevent multiples instances
         topCompaniesBtn.classList.add('loaded-element')
 
         // Init modals
-        const modals = M.Modal.init(document.querySelectorAll('.modal'));
-
+        M.Modal.init(document.querySelectorAll('.modal'));
         // Init tabs
         const tabs = M.Tabs.init(document.querySelectorAll('.tabs'));
 
-        // Listen when modal is open
+        // Listen when the modal is open
         topCompaniesBtn.addEventListener('click', () => {
+            // Update tab indicator
             window.setTimeout(() => {
                 tabs.forEach((tab) => {
                     tab.updateTabIndicator();
@@ -45,9 +49,6 @@ function domRefreshed() {
             }, 500);
         });
     }
-
-    // Get iframes reference
-    const iframes = document.querySelectorAll('iframe');
 
     iframes.forEach((iframe) => {
         if (!iframe.classList.contains('loaded-element')) {
@@ -58,6 +59,7 @@ function domRefreshed() {
              * Triggers when an iframe is loaded
              */
             const iframeLoaded = (event) => {
+                // If the iframe don't has window then was an error
                 if (event.target.contentWindow.window.length === 0) {
                     // Set fallback URL
                     iframe.src = iframe.dataset.fallback;
